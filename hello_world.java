3984bbef7e5f777144e9f88ee43d4b4ad31c09ed
fhillipbgsc <41215741+fhillipbgsc@users.noreply.github.com>
2019-03-27 11:58:20 +0800

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
   public static void main(String[] args) {
       System.out.println("HELLO WORLD!");
+    
+      System.out.println("I've been here!");
+      System.out.println("Fhillip Bagsic");
      }
  }
